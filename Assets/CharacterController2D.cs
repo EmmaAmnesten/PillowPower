ab20e3403b6c73b3bd7c61a115c71c1d60b207bc
Emma Amnesten <emma.amnesten@gmail.com>
2022-08-22 15:53:44 +0200

    Add smooth camera and remove stomp

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -7,18 +7,18 @@ public class CharacterController2D : MonoBehaviour
 	private Rigidbody2D playerRigidbody;
 	public float runPower;
 	public float jumpPower;
-	private bool isGrounded;
+	public bool isGrounded;
 
 	private void Awake(){
 		playerRigidbody = GetComponent<Rigidbody2D>();
-		isGrounded = false;
+		isGrounded = true;
 	}
 
 	public void Move(float xInput, float yInput){
 
 		playerRigidbody.AddForce(new Vector2(xInput * runPower,0));
 		
-		if (isGrounded){
+		if (isGrounded && yInput > 0){
 			playerRigidbody.AddForce(new Vector2(0 , yInput * jumpPower));
 		}
 
